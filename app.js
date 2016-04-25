aa6a701d56e1922de835d555233301e7b8c5a421
Miru5 <miru_katzy@yahoo.com>
2016-04-25 16:56:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,42 @@ app.get("/", function(req, res) {
   res.json({"message" : "Hey World!"});
 });
 
+app.get('/api/users', function(req, res) {
+    var username = req.param('username');
+    var password = req.param('password');
+    var a = " ";
+    var hash = "";
+    MongoClient.connect("mongodb://heroku_tn8g3mwx@ds013340.mlab.com:13340/heroku_tn8g3mwx", function(err, db) {
+        var users = db.collection("users")
+
+        //login
+        users.find({"username":username}).toArray(function (err, items) {
+        hash = items[0]["password"];
+            bcrypt.compare(password, hash, function(err, result) {
+                if(err==null){
+                    result === true
+                }
+               
+                if(result){
+
+                    activeUsers.push({"usr":items[0]["username"]})
+                    res.send(items);
+                }
+            });
+            // users.count({username:username}, function (err, count){
+            //     if(count>0){
+            //         res.send(items);
+            //     }
+            //     else {
+            //        res.send("error");
+            //     }
+            // });
+
+
+        });
+    });
+})
+
 // bind the app to listen for connections on a specified port
 var port = process.env.PORT || 3000;
 app.listen(port);
