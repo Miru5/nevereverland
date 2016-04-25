535599d4cfc8d808ea9cba14d79387f34af9736b
Miru5 <miru_katzy@yahoo.com>
2016-04-25 18:15:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,15 +10,9 @@ var uristring =
 // var MongoClient = require("mongodb").MongoClient
 // Set up a URL route
 
-mongo.MongoClient.connect(uristring, function (err, database) {
-  if (err) throw err;
-  db = database;
-  users = db.collection("Users");
-   users.find({"username":"xmy"}).toArray(function (err, items){
-        console.log('mere ',items[0]["password"]);
-  });
-  
-});
+
+
+
 
 // mongo.connect(uristring, {}, function(error, db){
 // var users = db.collection("Users");
@@ -48,11 +42,11 @@ app.get('/api/users', function(req, res) {
     var password = req.param('password');
     var a = " ";
     var hash = "";
-    MongoClient.connect("mongodb://heroku_tn8g3mwx@ds013340.mlab.com:13340/heroku_tn8g3mwx", function(err, db) {
-        var users = db.collection("users")
-
-        //login
-        users.find({"username":username}).toArray(function (err, items) {
+    mongo.MongoClient.connect(uristring, function (err, db) {
+  if (err) throw err;
+  var users = db.collection("Users");
+   users.find({"username":"xmy"}).toArray(function (err, items){
+        console.log('mere ',items[0]["password"]);
         hash = items[0]["password"];
             bcrypt.compare(password, hash, function(err, result) {
                 if(err==null){
@@ -77,7 +71,10 @@ app.get('/api/users', function(req, res) {
 
         });
     });
-})
+  
+});
+   
+      
 
 // bind the app to listen for connections on a specified port
 var port = process.env.PORT || 3000;
