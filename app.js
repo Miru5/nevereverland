29c98d6626cb42df3ff7da4d4e249130144c5312
Miru5 <miru_katzy@yahoo.com>
2016-04-25 16:25:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
  
 var server = http.createServer(function(req,res){
-res.WriteHead(200, {'Content-Type':'text/html'});
+res.writeHead(200, {'Content-Type':'text/html'});
 res.send('<h1>Hello World</h1>');
 }); 
 
