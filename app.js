4992611d6c7bce9dc251ae93f16f3ef1eb4939fa
Miru5 <miru_katzy@yahoo.com>
2016-04-25 18:26:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -37,21 +37,8 @@ app.get("/", function(req, res) {
   res.json({"message" : "Hey World!"});
 });
 
-app.get('/api/users', function(req, res) {
-   
-mongo.MongoClient.connect(uristring, function (err, db) {
-  if (err) throw err;
-//   var users = db.collection("Users");
-//   users.find({"username":"xmy"}).toArray(function (err, items){
-//         console.log('mere ',items[0]["password"]);
-//                     res.send(items);
-//                 }
-//             });
-//         });
-    });
 
-   
-      
+  
 
 // bind the app to listen for connections on a specified port
 var port = process.env.PORT || 3000;
