14bc153db078636fa4c6e59d09dc52199d818dfa
MirunaC# <miru_katzy@yahoo.com>
2016-05-09 17:45:17 +0300

    Update models.js

--- models.js
+++ models.js
@@ -2,13 +2,11 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;  
 
-var userSchema = mongoose.Schema({    
-     token : String,     
-     email: String,  
-     hashed_password: String,    
-     salt : String,  
-     temp_str:String 
-});  
+var userSchema = mongoose.Schema({
+    name : String,
+    mobno: String,
+    reg_id: String
+}); 
 
 mongoose.connect('mongodb://miru:toor@ds013340.mlab.com:13340/heroku_tn8g3mwx'); 
-module.exports = mongoose.model('users', userSchema);
+module.exports = mongoose.model('usersTest', userSchema);
