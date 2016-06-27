9c02b9c28d625d7c4d0d343996e2b13cff31826e
xMiruna5 <miru_katzy@yahoo.com>
2016-06-27 13:58:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ app.post('/api/setxp', function(req, res) {
             console.log(xid);
             users.update(
                 {'_id': new ObjectId(xid),"friends":{$elemMatch: {"username": player}}},
-                {$set: { "friends.$.xp" : xp} }
+                {$set: { "friends.$.xp" : { $inc: { xp:10}} }
             )
         }
         });
